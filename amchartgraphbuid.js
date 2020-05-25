885299fb68937a538893cbc701b03af3cfd62038
Yair Wygoda <yairwigi@gmail.com>
2020-05-25 17:37:24 +0300

    some testings..

--- amchartgraphbuid.js
+++ amchartgraphbuid.js
@@ -4,13 +4,43 @@ function buildGraph(info) {
   am4core.useTheme(am4themes_animated);
   var chart = am4core.create("chartdiv", am4plugins_forceDirected.ForceDirectedTree, am4charts.XYChart);
 //var chart = am4core.create("zoom",am4charts.XYChart);
+
+
+  //
+  // chart.legend = new am4charts.Legend();
+  // chart.legend.data = chart.data.map(i => ({
+  //   "name": i.title,
+  //   "fill": i.color
+  // }));
+
+
+
+
   chart.legend = new am4charts.Legend();
-  //chart.exporting.menu = new am4core.ExportMenu();
-  //chart.exporting.menu.container = document.getElementById("tools");
-  //chart.legend.parent = chart.chartContainer;
-  chart.legend.position = "right"
+  //
+  //
+  // //chart.exporting.menu = new am4core.ExportMenu();
+  // //chart.exporting.menu.container = document.getElementById("tools");
+  // //chart.legend.parent = chart.chartContainer;
+  //
+  //
+  //
+  chart.legend.position = "right";
   chart.legend.scrollable = true;
-  chart.legend.itemContainers.template.cursorOverStyle = am4core.MouseCursorStyle.default;
+  // chart.legend.itemContainers.template.cursorOverStyle = am4core.MouseCursorStyle.default;
+
+  // chart.legend.nodes.template.events.on("hit", function(ev) {
+  //   console.log("here in legendddddd");
+  //   console.log(ev.target);
+  //
+  // });
+  //
+  //
+
+
+
+
+
   chart.data.scrollable = true;
   chart.responsive.enabled = true;
   chart.resizable = true;
@@ -53,27 +83,72 @@ function buildGraph(info) {
 //networkSeries.dataFields.value = "value";
 //networkSeries.dataFields.value = "10";
   networkSeries.dataFields.children = "children";
-  networkSeries.dataFields.fixed="fixed"
+  networkSeries.dataFields.fixed="fixed";
   networkSeries.nodes.template.propertyFields.x = "x";
   networkSeries.nodes.template.propertyFields.y = "y";
   networkSeries.nodes.template.tooltipText = "{name}";
   networkSeries.nodes.template.fillOpacity = 1;
 //networkSeries.nodes.template.dataFields.url = "name";
-  networkSeries.nodes.template.label.text = "{name}"
-//networkSeries.dataFields.collapsed = "true";
-//networkSeries.links.template.distance = 2;
+  networkSeries.nodes.template.label.text = "{name}";
+
+
+  //////////////////////////////
+  // networkSeries.dataFields.collapsed = "true";
+  // networkSeries.dataFields.collapsed = "false";
+
+
+  ////////////////////////////////
+
+  //networkSeries.links.template.distance = 2;
+
   networkSeries.manyBodyStrength = -30;
 //networkSeries.links.template.distance = 1;
 //networkSeries.links.template.strength = 1;
   networkSeries.nodes.template.expandAll = false;
   networkSeries.fontSize = 8;
+
   networkSeries.currentLevel = 1;
+
+
+
+
+  // networkSeries.maxLevels = 0;
   networkSeries.maxLevels = 2;
+  // networkSeries.nodes.each ((node) => {node.isActive = true;});
+
+
+
+  // Close other nodes when one is opened
+  networkSeries.nodes.template.events.on("hit", function(ev) {
+    var targetNode = ev.target;
+    if (targetNode.isActive) {
+      networkSeries.nodes.each(function(node) {
+        if (targetNode !== node && node.isActive && targetNode.dataItem.level === node.dataItem.level) {
+          node.isActive = false;
+        }
+      });
+    }
+  });
+
+  // networkSeries.legendSettings.createMarker= false;
+
+
+
   networkSeries.maxRadius = 50;
   networkSeries.minRadius = 25;
   networkSeries.nodes.template.label.hideOversized = true;
   networkSeries.nodes.template.label.truncate = true;
+
+
+
+
+  ////////////////////////
   networkSeries.dataFields.collapsed = "off";
+  //////////////////////////////
+
+
+
+
   /*networkSeries.nodes.template.adapter.add("fill", function (fill, target) {
     return fill.lighten(target.dataItem.level * 0.25);
   });*/
@@ -101,6 +176,15 @@ function buildGraph(info) {
       event.target.dataItem.parentLink.isHover = false;
     }
   })
+
+
+  // networkSeries.events.on("inited", function() {
+  //   networkSeries.animate({
+  //     property: "velocityDecay",
+  //     to: 1
+  //   }, 3000);
+  // });
+
 
 }
 /*networkSeries.nodes.template.events.on("hit", function(ev) {
