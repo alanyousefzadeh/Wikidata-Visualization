a6a7407c104bda1f7c65990681a7173e23e9a5c3
Yair Wygoda <yairwigi@gmail.com>
2020-07-02 12:35:34 +0300

    css modifications

--- amchartgraphbuid.js
+++ amchartgraphbuid.js
@@ -1,6 +1,17 @@
 var legend  = null;
+
+am4core.useTheme(am4themes_animated);
+
+
+
+
 var chart = am4core.create("chartdiv", am4plugins_forceDirected.ForceDirectedTree);
+
 chart.logo.disabled = true;
+
+
+
+
 
 var networkSeries = null;
 function buildGraph(info) {
@@ -12,79 +23,37 @@ function buildGraph(info) {
   }
   console.log(info)
   info=JSON.parse(info)
-  am4core.useTheme(am4themes_animated);
+  // am4core.useTheme(am4themes_animated);
 //var chart = am4core.create("zoom",am4charts.XYChart);
 
   chart.logo.disabled = true;
 
-  //
-  // chart.legend = new am4charts.Legend();
-  // chart.legend.data = chart.data.map(i => ({
-  //   "name": i.title,
-  //   "fill": i.color
-  // }));
-
-  chart.legend = new am4charts.Legend();
-  legend = chart.legend;
-
-  //
-  //
-  // //chart.exporting.menu = new am4core.ExportMenu();
-  // //chart.exporting.menu.container = document.getElementById("tools");
-  // //chart.legend.parent = chart.chartContainer;
-  //
-  //
-  //
-  chart.legend.position = "right";
-  chart.legend.scrollable = true;
-  // chart.legend.itemContainers.template.cursorOverStyle = am4core.MouseCursorStyle.default;
-
-  // chart.legend.dataItem.events.on()("hit", function(ev) {
-  //   var targetNode = ev.target;
-  //   if (targetNode.isActive) {
-  //     networkSeries.nodes.each(function(node) {
-  //       if (targetNode !== node && node.isActive && targetNode.dataItem.level === node.dataItem.level) {
-  //         node.isActive = false;
-  //       }
-  //     });
-  //   }
-  // });
 
   chart.data.scrollable = true;
   chart.responsive.enabled = true;
   chart.resizable = true;
-   networkSeries = chart.series.push(new am4plugins_forceDirected.ForceDirectedSeries())
+  networkSeries = chart.series.push(new am4plugins_forceDirected.ForceDirectedSeries());
   networkSeries.scrollbarX = new am4charts.XYChartScrollbar();
   networkSeries.scrollbarX.series.push(networkSeries);
 
   networkSeries.data = [info];
-  console.log(networkSeries.data)
 
-  /*var zoomInButton = buttonContainer.createChild(am4core.Button);
-  zoomInButton.label.text = "+";
-  zoomInButton.events.on("hit", function(ev) {
-    var diff = dateAxis.maxZoomed - dateAxis.minZoomed;
-    var delta = diff * 0.2;
-    dateAxis.zoomToDates(new Date(dateAxis.minZoomed + delta), new Date(dateAxis.maxZoomed - delta));
-  });
 
-  var zoomOutButton = buttonContainer.createChild(am4core.Button);
-  zoomOutButton.label.text = "-";
-  zoomOutButton.events.on("hit", function(ev) {
-    var diff = dateAxis.maxZoomed - dateAxis.minZoomed;
-    var delta = diff * 0.2;
-    dateAxis.zoomToDates(new Date(dateAxis.minZoomed - delta), new Date(dateAxis.maxZoomed + delta));
-  });*/
+  /* Create series */
+  /* Create legend */
+  chart.legend = new am4charts.Legend();
+  legend = chart.legend;
+  chart.legend.position = "right";
 
-  /*let icon = networkSeries.nodes.template.createChild(am4core.Triangle);
-  //icon.href = "triangle-icon.png";
-  icon.horizontalCenter = "middle";
-  icon.verticalCenter = "middle";
-  icon.width = 40;
-  icon.height = 40;
+  chart.legend.scrollable = true;
+  // /* Create a separate container to put legend in */
+  // var legendContainer = am4core.create("legenddiv", am4core.Container);
+  // legendContainer.width = am4core.percent(20);
+  // legendContainer.height = am4core.percent(100);
+  // chart.legend.parent = legendContainer;
 
-  networkSeries.nodes.template.circle.disabled = true;
-  networkSeries.nodes.template.outerCircle.disabled = true;*/
+
+  console.log(networkSeries.data);
 
   networkSeries.dataFields.linkWith = "linkWith";
   networkSeries.dataFields.name = "name";
@@ -99,18 +68,8 @@ function buildGraph(info) {
   networkSeries.nodes.template.fillOpacity = 1;
 //networkSeries.nodes.template.dataFields.url = "name";
   networkSeries.nodes.template.label.text = "{name}";
-
-
-  //////////////////////////////
-  // networkSeries.dataFields.collapsed = "true";
-  // networkSeries.dataFields.collapsed = "false";
-
-
-  ////////////////////////////////
-
-  //networkSeries.links.template.distance = 2;
-
-  networkSeries.manyBodyStrength = -30;
+  networkSeries.centerStrength = 3;
+  networkSeries.manyBodyStrength = -50;
 //networkSeries.links.template.distance = 1;
 //networkSeries.links.template.strength = 1;
   networkSeries.nodes.template.expandAll = false;
@@ -118,14 +77,9 @@ function buildGraph(info) {
 
   networkSeries.currentLevel = 1;
 
-
-
-
   // networkSeries.maxLevels = 0;
   networkSeries.maxLevels = 2;
   // networkSeries.nodes.each ((node) => {node.isActive = true;});
-
-
 
   // Close other nodes when one is opened
   networkSeries.nodes.template.events.on("hit", function(ev) {
@@ -139,7 +93,6 @@ function buildGraph(info) {
     }
   });
 
-
   // Close other nodes when one is opened in the legend
   chart.legend.itemContainers.template.events.on("hit", function(event) {
     var targetNode = event.target;
@@ -152,41 +105,17 @@ function buildGraph(info) {
         }
       });
     }
-    // for(var i = 0; i<legend.itemContainers.values.length ; i++){
-    //   if (targetNode !== legend.itemContainers.values[i].isActive && legend.itemContainers.values[i].isActive) {
-    //     legend.itemContainers.values[i].isActive = true;
-    //
-    //   }
-    // }
-    // targetNode.isActive = false;
 
   });
-
-
-  // networkSeries.legendSettings.createMarker= false;
-
-
 
   networkSeries.maxRadius = 50;
   networkSeries.minRadius = 25;
   networkSeries.nodes.template.label.hideOversized = true;
   networkSeries.nodes.template.label.truncate = true;
 
-
-
-
   ////////////////////////
   networkSeries.dataFields.collapsed = "off";
   //////////////////////////////
-
-
-
-
-  /*networkSeries.nodes.template.adapter.add("fill", function (fill, target) {
-    return fill.lighten(target.dataItem.level * 0.25);
-  });*/
-
-
   var hoverState = networkSeries.links.template.states.create("hover");
   hoverState.properties.strokeWidth = 3;
   hoverState.properties.strokeOpacity = 1;
@@ -194,89 +123,21 @@ function buildGraph(info) {
   networkSeries.nodes.template.events.on("over", function (event) {
     event.target.dataItem.childLinks.each(function (link) {
       link.isHover = true;
-    })
+    });
     if (event.target.dataItem.parentLink) {
       event.target.dataItem.parentLink.isHover = true;
     }
 
-  })
+  });
 
   networkSeries.nodes.template.events.on("out", function (event) {
     event.target.dataItem.childLinks.each(function (link) {
       link.isHover = false;
-    })
+    });
     if (event.target.dataItem.parentLink) {
       event.target.dataItem.parentLink.isHover = false;
     }
   })
 
 
-  // networkSeries.events.on("inited", function() {
-  //   networkSeries.animate({
-  //     property: "velocityDecay",
-  //     to: 1
-  //   }, 3000);
-  // });
-
-
 }
-/*networkSeries.nodes.template.events.on("hit", function(ev) {
-  var targetNode = ev.target;
-  if (targetNode.isActive) {
-    networkSeries.nodes.each(function(node) {
-      if (targetNode !== node && node.isActive && targetNode.dataItem.level == node.dataItem.level) {
-        node.isActive = false;
-      }
-    });
-  }
-});*/
-
-/*networkSeries.nodes.template.adapter.add("tooltipText", function(text, target) {
-  if (target.dataItem) {
-    switch(target.dataItem.level) {
-      case 0:
-        return "#1: {name}";
-      case 1:
-        return "#2: {parent.name} > {name} ({value})";
-      case 2:
-        return "#3: {parent.parent.name} > {parent.name} > {name} ({value})";
-    }
-  }
-  return text;
-});
-
-
-/*networkSeries.dataFields.value = "value";
-networkSeries.dataFields.name = "name";
-networkSeries.dataFields.children = "children";
-networkSeries.nodes.template.tooltipText = "{name}:{value}";
-networkSeries.nodes.template.fillOpacity = 1;
-
-networkSeries.nodes.template.label.text = "{name}"
-networkSeries.fontSize = 10;
-
-networkSeries.links.template.strokeWidth = 1;
-
-var hoverState = networkSeries.links.template.states.create("hover");
-hoverState.properties.strokeWidth = 3;
-hoverState.properties.strokeOpacity = 1;
-
-networkSeries.nodes.template.events.on("over", function(event) {
-  event.target.dataItem.childLinks.each(function(link) {
-    link.isHover = true;
-  })
-  if (event.target.dataItem.parentLink) {
-    event.target.dataItem.parentLink.isHover = true;
-  }
-
-})
-
-networkSeries.nodes.template.events.on("out", function(event) {
-  event.target.dataItem.childLinks.each(function(link) {
-    link.isHover = false;
-  })
-  if (event.target.dataItem.parentLink) {
-    event.target.dataItem.parentLink.isHover = false;
-  }
-})
-*/
